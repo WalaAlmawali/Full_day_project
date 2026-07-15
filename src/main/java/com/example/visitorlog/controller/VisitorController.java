9bad776cb43e2f24b32be9550cd5fdd4d599c954
Wala AL mawali <wala.salam.9888@gmail.com>
2026-07-15 10:03:56 +0400

    Add getAllVisitors() and getVisitorById(@PathVariable Long id) functions

--- src/main/java/com/example/visitorlog/controller/VisitorController.java
+++ src/main/java/com/example/visitorlog/controller/VisitorController.java
@@ -1,9 +1,40 @@
 package com.example.visitorlog.controller;
 
+import com.example.visitorlog.model.Visitor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 @RequestMapping("/api/visitors")
 public class VisitorController {
+
+    private List<Visitor> visitors = new ArrayList<>();
+    private AtomicLong counter= new AtomicLong();
+
+
+    @GetMapping
+    public List<Visitor> getAllVisitors(){
+        return visitors;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Visitor> getVisitorById(@PathVariable Long id) {
+        for (Visitor visitor : visitors) {
+            if (visitor.getId().equals(id)) {
+                return ResponseEntity.ok(visitor);
+            }
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+
+
+
 }
