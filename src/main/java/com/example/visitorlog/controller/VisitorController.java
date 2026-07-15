97327b0e5bd4c969de9a706dadf8e2726dd115a4
Wala AL mawali <wala.salam.9888@gmail.com>
2026-07-15 10:09:07 +0400

    Add addVisitor(@RequestBody Visitor visitor) function

--- src/main/java/com/example/visitorlog/controller/VisitorController.java
+++ src/main/java/com/example/visitorlog/controller/VisitorController.java
@@ -2,14 +2,13 @@ package com.example.visitorlog.controller;
 
 import com.example.visitorlog.model.Visitor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
+
+import static com.sun.tools.javac.jvm.ByteCodes.ret;
 
 @RestController
 @RequestMapping("/api/visitors")
@@ -32,6 +31,13 @@ public class VisitorController {
             }
         }
         return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Visitor> addVisitor(@RequestBody Visitor visitor){
+        visitor.setId(counter.incrementAndGet());
+        visitors.add(visitor);
+        return ResponseEntity.ok(visitor);
     }
 
 
