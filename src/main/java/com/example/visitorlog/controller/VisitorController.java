b9164968a52331d32b31e4ddb2f1f471f1cf373c
Wala AL mawali <wala.salam.9888@gmail.com>
2026-07-15 09:52:07 +0400

    Add @RestController and @RequestMapping

--- src/main/java/com/example/visitorlog/controller/VisitorController.java
+++ src/main/java/com/example/visitorlog/controller/VisitorController.java
@@ -1,4 +1,9 @@
 package com.example.visitorlog.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/visitors")
 public class VisitorController {
 }
